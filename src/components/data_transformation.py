8c4064dee508650eb93e31fed5ebe3d81a43d27a
Ujjwal Bodkhe <ujjwal.bodkhe@gmail.com>
2024-08-10 23:11:36 +0530

    Prediction Pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,7 +13,7 @@ from src.logger import logging
 from src.utils import save_object
 @dataclass
 class DataTransFormationConfig:
-    preprocessor_obj_file_path = os.path.join('artifact',"preprocessor.pkl")
+    preprocessor_obj_file_path = os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self): 
